9f7932b546963e10f7cfeb7fae9f5950c383ce47
Tuija <tuija.sonkkila@aalto.fi>
2022-04-15 18:21:14 +0300

    Link to tweet

--- biv_and_patt.R
+++ biv_and_patt.R
@@ -3,6 +3,7 @@ library(ggpattern)
 library(biscale)
 library(cowplot)
 
+# https://twitter.com/jhilden/status/1513882835937026073
 # https://slu-opengis.github.io/biscale/articles/biscale.html
 data <- bi_class(stl_race_income, x = pctWhite, y = medInc, style = "quantile", dim = 3)
 
