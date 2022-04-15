2db4bded6b90d8f8041c799a890e8b3bba3f8414
Tuija <tuija.sonkkila@aalto.fi>
2022-04-15 18:15:38 +0300

    Initial commit

---
+++ biv_and_patt.R
@@ -0,0 +1,29 @@
+library(tidyverse)
+library(ggpattern)
+library(biscale)
+library(cowplot)
+
+# https://slu-opengis.github.io/biscale/articles/biscale.html
+data <- bi_class(stl_race_income, x = pctWhite, y = medInc, style = "quantile", dim = 3)
+
+data20 <- data %>% 
+  slice_head(n = 20) %>% 
+  mutate(bi_class = as.factor(bi_class))
+
+map_biv_patt <- ggplot(data20) +
+  geom_sf_pattern(aes(pattern = bi_class, pattern_fill = bi_class)) +
+  bi_theme() +
+  theme(legend.position = "none")
+
+map_biv_patt_color <- ggplot(small) +
+  geom_sf_pattern(aes(pattern = bi_class, pattern_fill = bi_class)) +
+  geom_sf(data = small, mapping = aes(fill = bi_class, alpha = 0.1)) +
+  bi_scale_fill(pal = "DkBlue", dim = 3) +
+  bi_theme() +
+  theme(legend.position = "none")
+
+maps <- plot_grid(map_biv_patt, map_biv_patt_color)
+
+maps
+
+
