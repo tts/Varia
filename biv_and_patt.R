8401e36af182d5c680f9a5a61d8f0bc62d4d364a
Tuija <tuija.sonkkila@aalto.fi>
2022-04-16 20:52:06 +0300

    Legends

--- biv_and_patt.R
+++ biv_and_patt.R
@@ -15,19 +15,19 @@ data20 <- data %>%
 map_biv_patt1 <- ggplot(data20) +
   geom_sf_pattern(aes(pattern = bi_class)) +
   bi_theme() +
-  theme(legend.position = "none")
+  theme(legend.position = "none") 
 
 # pattern and fill by class
 map_biv_patt2 <- ggplot(data20) +
   geom_sf_pattern(aes(pattern = bi_class, pattern_fill = bi_class)) +
   bi_theme() +
-  theme(legend.position = "none")
+  theme(legend.position = "none") 
 
 # pattern and fill by class plus biscale fill
 map_biv_patt2_color <- ggplot(data20) +
   geom_sf_pattern(aes(pattern = bi_class, pattern_fill = bi_class)) +
   geom_sf(mapping = aes(fill = bi_class, alpha = 0.1)) +
-  bi_scale_fill(pal = "DkBlue", dim = 3) +
+  bi_scale_fill(pal = "DkBlue", dim = 3)  +
   bi_theme() +
   theme(legend.position = "none")
 
@@ -36,4 +36,25 @@ maps <- plot_grid(map_biv_patt1, map_biv_patt2, map_biv_patt2_color, ncol = 3)
 maps
 
 ggsave("maps.png", width = 60, height = 20, dpi = 72, units = "cm", device = "png")
+
+# The last map with legends (a rough one)
+map_biv_patt2_color <- ggplot(data20) +
+  geom_sf_pattern(aes(pattern = bi_class, pattern_fill = bi_class)) +
+  geom_sf(mapping = aes(fill = bi_class, alpha = 0.1), show.legend = FALSE) +
+  bi_scale_fill(pal = "DkBlue", dim = 3)  +
+  theme_void() 
+
+legend <- bi_legend(pal = "DkBlue",
+                    dim = 3,
+                    xlab = "Higher % White ",
+                    ylab = "Higher Income ",
+                    size = 8)
+
+comboPlot <- ggdraw() +
+  draw_plot(map_biv_patt2_color, 0, 0, 1, 1) +
+  draw_plot(legend, 0.75, 0.2, 0.2, 0.2)
+
+comboPlot
+
+ggsave("comboPlot.png", width = 30, height = 30, dpi = 72, units = "cm", device = "png")
 
