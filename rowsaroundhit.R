5ebed5a1ea042837d6f2198ea8d53bdf67726d17
Tuija <tuija.sonkkila@aalto.fi>
2022-08-15 19:53:57 +0300

    Initial commit

---
+++ rowsaroundhit.R
@@ -0,0 +1,20 @@
+# https://twitter.com/jhilden/status/1557368903885635584
+# filtering all rows where a condition is fulfilled 
+# PLUS the following and preceding rows
+
+column_1 <- c(1:5)
+column_2 <- c(1:5)
+my_column <- c(1:3,10,5)
+
+d <- data.frame(column_1, column_2, my_column)
+
+# base R
+d[which(d$my_column == 10) + c(-1:1), ]
+
+# dplyr
+library(dplyr)
+d %>% 
+  filter( (my_column == 10) | 
+            lead(my_column == 10) | 
+            lag(my_column == 10)
+          )
