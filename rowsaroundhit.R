70fb0376676a998402c13821425a659b0c81944c
Tuija <tuija.sonkkila@aalto.fi>
2022-08-15 20:17:31 +0300

    dplyr only

--- rowsaroundhit.R
+++ rowsaroundhit.R
@@ -2,19 +2,15 @@
 # filtering all rows where a condition is fulfilled 
 # PLUS the following and preceding rows
 
-column_1 <- c(1:5)
-column_2 <- c(1:5)
-my_column <- c(1:3,10,5)
-
+column_1 <- c(1:10)
+column_2 <- c(1:10)
+my_column <- c(1:2,100,4:8,100,10)
 d <- data.frame(column_1, column_2, my_column)
-
-# https://stackoverflow.com/a/13155669
-d[which(d$my_column == 10) + c(-1:1), ]
 
 # https://stackoverflow.com/a/54349439
 library(dplyr)
 d %>% 
-  filter( (my_column == 10) | 
-            lead(my_column == 10) | 
-            lag(my_column == 10)
+  filter( (my_column == 100) | 
+            lead(my_column == 100) | 
+            lag(my_column == 100)
           )
