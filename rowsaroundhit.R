c3afa6efd19cf22e40cbff449ac827210f2e7d6a
Tuija <tuija.sonkkila@aalto.fi>
2022-08-15 21:58:46 +0300

    More than one row

--- rowsaroundhit.R
+++ rowsaroundhit.R
@@ -2,15 +2,22 @@
 # filtering all rows where a condition is fulfilled 
 # PLUS the following and preceding rows
 
-column_1 <- c(1:10)
-column_2 <- c(1:10)
-my_column <- c(1:2,100,4:8,100,10)
+column_1 <- c(1:11)
+column_2 <- c(1:11)
+my_column <- c(1:2,100,4:8,100,10:11)
 d <- data.frame(column_1, column_2, my_column)
 
 # https://stackoverflow.com/a/54349439
 library(dplyr)
 d %>% 
   filter( (my_column == 100) | 
-            lead(my_column == 100) | 
-            lag(my_column == 100)
+            lead(my_column == 100, n = 2) |
+            lag(my_column == 100, n = 2)
           )
+
+# More than one row, eg. 2?
+# https://stackoverflow.com/a/44100311
+inds = which(d$my_column == 100)
+rows <- lapply(inds, function(x) (x-2):(x+2))
+d[unlist(rows),]
+
